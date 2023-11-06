e0217976896c63a537e47f71ebb2836774d20351
sheikhjaber0424 <sheikhjaber190@gmail.com>
2023-11-06 22:24:16 +0600

    image path update

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -5,17 +5,17 @@ import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 function Gallery() {
   const [images, setImages] = useState([
-    { image: "/src/images/image-1.webp", id: 1, checked: false },
-    { image: "/src/images/image-2.webp", id: 2, checked: false },
-    { image: "/src/images/image-3.webp", id: 3, checked: false },
-    { image: "/src/images/image-4.webp", id: 4, checked: false },
-    { image: "/src/images/image-5.webp", id: 5, checked: false },
-    { image: "/src/images/image-6.webp", id: 6, checked: false },
-    { image: "/src/images/image-7.webp", id: 7, checked: false },
-    { image: "/src/images/image-8.webp", id: 8, checked: false },
-    { image: "/src/images/image-9.webp", id: 9, checked: false },
-    { image: "/src/images/image-10.jpeg", id: 10, checked: false },
-    { image: "/src/images/image-11.jpeg", id: 11, checked: false },
+    { image: "./src/images/image-1.webp", id: 1, checked: false },
+    { image: "./src/images/image-2.webp", id: 2, checked: false },
+    { image: "./src/images/image-3.webp", id: 3, checked: false },
+    { image: "./src/images/image-4.webp", id: 4, checked: false },
+    { image: "./src/images/image-5.webp", id: 5, checked: false },
+    { image: "./src/images/image-6.webp", id: 6, checked: false },
+    { image: "./src/images/image-7.webp", id: 7, checked: false },
+    { image: "./src/images/image-8.webp", id: 8, checked: false },
+    { image: "./src/images/image-9.webp", id: 9, checked: false },
+    { image: "./src/images/image-10.jpeg", id: 10, checked: false },
+    { image: "./src/images/image-11.jpeg", id: 11, checked: false },
   ]);
 
   const handleDelete = () => {
